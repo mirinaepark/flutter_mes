b21a724e6818cf3a8d86b77263926ef17aa9788c
rinae <goroweon@gmail.com>
2023-10-31 09:41:59 +0900

    drawer - Listview를 container로 감싸서 전체 색깔 적용 - drawerHeader를 container로 감싸서 높이 적용 -레이아웃 이해하는 중...

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,12 @@ void main() {
   runApp(const MyApp());
 }
 
-
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     FlutterNativeSplash.remove();
 
     return MaterialApp(
@@ -27,55 +24,80 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'splash',),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('mrn MES'),
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: Colors.grey[400],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      drawer: Drawer(
+        child: Container(
+          color: Colors.grey[350],
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              Container(
+                height: 130,
+                child: const DrawerHeader(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('사원명 :'),
+                      Text('사원번호 :'),
+                    ],
+                  ),
+                ),
+              ),
+              const ListTile(
+                leading: Icon(Icons.add),
+                title: Text('생산 현장 조회'),
+              ),
+              const ListTile(
+                leading: Icon(Icons.add),
+                title: Text('생산 일정 관리'),
+              ),
+              const ListTile(
+                leading: Icon(Icons.add),
+                title: Text('생산 현황 조회'),
+              ),
+              const ListTile(
+                leading: Icon(Icons.add),
+                title: Text('재고 관리'),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: DefaultTextStyle.merge(
+        style: const TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
+        child: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('미리내'),
+              Text('화이팅'),
+              Text('너는'),
+              Text('할 수 있어'),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
-
